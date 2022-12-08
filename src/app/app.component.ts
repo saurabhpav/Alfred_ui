fccda491fefc745c562868bfc316b66198c700e6
saurabhpav <120093700+saurabhpav@users.noreply.github.com>
2022-12-08 15:48:30 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'imgupload';
+  url = '';
+
+onselect(event:any){
+   if(event.target.files){
+      var reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.onload=(event:any)=>{
+        this.url=event.target.result;
+    }
+  }
+}
+
+
+}
